f30ac8ecd070b2c6e20c6f860c30e95f01987a03
Jolly <147080381+Jollychuks@users.noreply.github.com>
2026-09-09 16:41:59 -0400

    Delete chembreak7 directory

--- chembreak7/scripts/preflight.py
+++
@@ -1,9 +0,0 @@
-from chembreak7.preflight import run_preflight
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("config")
-    args = parser.parse_args()
-    print(run_preflight(args.config))
--- chembreak7/src/chembreak7/__init__.py
+++
@@ -1,4 +0,0 @@
-"""ChemBreak7."""
-
-__version__ = "7.0.0"
-RUN_NAMESPACE = "CB7"
