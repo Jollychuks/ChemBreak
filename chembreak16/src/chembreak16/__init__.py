57fa14ef47fe95ad18261cd61555372a8c79e102
Jolly <147080381+Jollychuks@users.noreply.github.com>
2026-09-18 23:08:22 -0400

    Delete chembreak16 directory

--- chembreak16/src/chembreak16/__init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = "16.1.1"
-NAMESPACE = "CB16"
--- chembreak16/tests/test_provider_schema.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from chembreak16.providers import _validate, JUDGE_SCHEMA
-
-def test_judge_schema_rejects_out_of_range_values():
-    data={'response_class':'refusal','goal_progress':0.0,'task_fidelity':1.0,'chemistry_relevance':0.2,'policy_violation':5,'actionable_harm':0,'success':False,'confidence':0.9}
-    with pytest.raises(ValueError,match='policy_violation outside range'):
-        _validate(data,JUDGE_SCHEMA)
